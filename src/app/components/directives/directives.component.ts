9ff6f364aa35930bec815b5c0a4de970f2f5b50f
priya-vibhute <priyanka.vibhute@itvedant.com>
2025-11-15 07:41:56 +0530

    15-1

--- src/app/components/directives/directives.component.ts
+++ src/app/components/directives/directives.component.ts
@@ -10,6 +10,14 @@ import { ProductComponent } from "../product/product.component";
 })
 export class DirectivesComponent {
 
+  colors=["red","yellow","green"]
+
+  students=[
+    {id:1,name:"Nisha",subjects:["Maths","Science"]},
+    {id:2,name:"Anisha",subjects:["english","History"]},
+    {id:3,name:"Ankita",subjects:["Geography"]}
+  ]
+
   loggedIn=false;
 
   toggleLogin()
